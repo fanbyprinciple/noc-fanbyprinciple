fdfca62a40dd5e8f8f3a5c8ad917fda3d58cc38d
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-07-28 23:55:16 +0530

    Initial commit

---
+++ introduction/blackboard/sketch.js
@@ -0,0 +1,42 @@
+
+
+function setup (){
+    createCanvas(400,300)
+    c = new Chalk()
+    background(51)
+     
+
+}
+
+function draw(){
+    
+    c.step()
+    c.display()
+    
+
+}
+
+class Chalk {
+    constructor(){
+        this.x =width/2
+        this.y =height/2
+    }
+
+    display(){
+        stroke (255)
+        point (this.x,this.y)
+    }
+
+    step(){
+        
+        let stepx = floor(random(3))-1
+
+        let stepy = floor(random(3))-1 
+
+        console.log(stepx,",",stepy)
+
+        this.x += stepx
+        this.y += stepy
+
+    }
+}
