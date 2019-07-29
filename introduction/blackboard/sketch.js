70a840e9a4d89269f79a816c6e3781a85b0a9548
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-07-29 22:55:19 +0530

    blackboard:now the chalk follows you

--- introduction/blackboard/sketch.js
+++ introduction/blackboard/sketch.js
@@ -4,7 +4,21 @@ function setup (){
     createCanvas(400,300)
     c = new Chalk()
     background(51)
-     
+    // stuff =[]
+    // stuff[0] = 1
+    // stuff[1] = 1
+    // stuff[2] = 2
+    // stuff[3] = 3
+    // stuff[4] = 3
+    // index = random(stuff)
+    // console.log(index)
+
+    // prob = 0.10
+    // r=random(1)
+    // if(r<prob){
+    //     console.log("gotcha")
+    // }
+
 
 }
 
@@ -29,14 +43,41 @@ class Chalk {
 
     step(){
         
-        let stepx = floor(random(4))-1
+        // let stepx = floor(random(4))-1
 
-        let stepy = floor(random(4))-1 
+        // let stepy = floor(random(4))-1 
 
-        console.log(stepx,",",stepy)
+        // //console.log(stepx,",",stepy)
 
-        this.x += stepx
+        // this.x += stepx
+        // this.y += stepy
+
+        let mouse_x =  mouseX - this.x
+        let mouse_y = mouseY - this.y
+
+        let stepx=0
+        let stepy= 0
+
+        
+
+        
+        let r = random(1)
+        if(r <0.5){
+            if(mouse_x && mouse_y){
+                stepx = (mouse_x/Math.abs(mouse_x)) 
+                stepy = (mouse_y/Math.abs(mouse_y)) 
+            }
+
+        } else {
+            stepx = floor(random(3))-1
+            stepy = floor(random(3))-1
+        } 
+
         this.y += stepy
+        this.x += stepx
+
+        //console.log("step: ",stepx,",",stepy)
+        //console.log("this.pos: ",stepx,",",stepy)
 
     }
 }
