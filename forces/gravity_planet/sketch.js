0fbf2b25cea1582f29b19a62921088d8779a7240
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-08-13 01:14:46 +0530

    gravity_planet: planet ecosystem

--- forces/gravity_planet/sketch.js
+++ forces/gravity_planet/sketch.js
@@ -1,3 +1,6 @@
+
+var G = 1
+
 class Planet {
     constructor(mass,x,y) {
         this.mass = mass
@@ -16,7 +19,7 @@ class Planet {
         distance = constrain(distance, 5.0, 25.0)
         f.normalize()
 
-        let G = 1
+        
         let strength = (G * this.mass * m.mass)/ (distance * distance)
         f.mult(strength)
         return f
@@ -64,29 +67,60 @@ class Comet {
             this.loc.y = height
         }
     }
+
+    attract(m) {
+        let f = p5.Vector.sub(this.loc, m.loc)
+        let dist = f.mag()
+        dist = constrain(dist,5.0,25.0)
+        
+        f.normalize()
+
+        let strength = (G * this.mass * m.mass)/ (dist*dist)
+        f.mult(strength)
+        return f
+    }
 }
 
-var comet 
+var comets = []
 var planet 
 
 function setup () {
     createCanvas(200,200)
-    comet = new Comet(10,0,0)
+    for(let i =0; i < 10; i++){
+        comets[i] = new Comet(random(10),random(0,width),random(0,height))
+    }    
     planet = new Planet(20,width/2,height/2)
+    
 }
 
 function draw() {
     background(0)
-
-    let force = planet.attract(comet)
+    
     let wind = createVector(0.01,0)
     let gravity = createVector(0,-0.1)
-    comet.applyForce(force)
-    comet.applyForce(wind)
-    comet.applyForce(gravity)
 
-    comet.update()
+    
+    for(let i =0 ; i < 10; ++i) {
+
+        let f_planet = planet.attract(comets[i])
+        comets[i].applyForce(f_planet)
+
+        
+        for (let j=0 ; j <10; j++){
+            let f_comet = comets[i].attract(comets[j])
+            comets[i].applyForce(f_comet)
+        }
+        
+        
+        comets[i].applyForce(wind)
+        comets[i].applyForce(gravity)
+
+        comets[i].update()
+        comets[i].display()
+    }    
+        
+    
 
     planet.display()
-    comet.display()
+    
 }
