02f414407a4951d3afd5d4d43ca4b17e57c39b09
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-08-13 12:38:12 +0530

    agar_io: debuggin done,blob growing

--- extra_projects/agar_io/sketch.js
+++ extra_projects/agar_io/sketch.js
@@ -2,6 +2,7 @@ function Blob(r,x,y,color) {
     this.pos = createVector(x,y)
     this.radius = r
     this.color = color
+    this.vel = createVector(0,0)
     this.show = function() {
         fill (this.color)
         //console.log(this.pos.x,this.radius)
@@ -9,21 +10,42 @@ function Blob(r,x,y,color) {
     }
 
     this.update = function(){
-        var mousee =createVector(mouseX,mouseY)
-        mousee.sub (this.pos)
-        mousee.setMag(3)
-        this.pos.add(mousee)
+        let newvel = createVector(mouseX- width/2,mouseY-height/2)
+        newvel.setMag(3)
+        this.vel.lerp(newvel,0.2)
+        this.pos.add(this.vel)
+    }
+
+    this.eats = function(other){
+        let d = p5.Vector.dist(this.pos,other.pos)
+        let sum = PI * this.radius *this.radius + PI * other.radius *other.radius
+        
+        if(d < (this.radius + other.radius)){
+            //console.log(sqrt(sum/PI))
+            this.radius  = sqrt (sum/PI)
+            console.log(this.radius,",",d)
+            
+
+            //this.radius += other.radius* 0.02
+            return true
+        } else {
+            return false
+        }
     }
 }
 
 var blobs = []
+var no_of_blobs = 200
 var myBlob
+var zoom = 1
 
 function setup() {
-    createCanvas(300,300)
+    createCanvas(600,600)
     myBlob = new Blob(64,width/2,height/2,255)
-    for (let i=0; i <10; ++i){
-        blobs[i] = new Blob(16,random(width),random(height),250)
+    for (let i=0; i < no_of_blobs; ++i){
+        let x = random(-width,width)
+        let y = random(-height, height)
+        blobs[i] = new Blob(16,x,y,250)
     }
     
     
@@ -31,10 +53,22 @@ function setup() {
 
 function draw() {
     background(0)
+    translate(width/2,height/2-myBlob.pos.y)
     myBlob.show()
     myBlob.update()
-    for (let i = 0; i <10; ++i){
-        blobs[i].show()
+    for (let i = no_of_blobs - 1; i >= 0; --i){
+
+        if(blobs[i]){
+            blobs[i].show()
+            if(myBlob.eats(blobs[i])){
+                console.log("one gone")
+                blobs.splice(i,1)
+            
+            //myBlob.grow()
+
+            }
+        } 
+        
     }
     
 }
