18a1a18814a46ac097f19ac05492b428a1dc4061
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-08-14 13:14:07 +0530

    socket_canvas: server sending client recieveing

--- extra_projects/socket_canvas/public/sketch.js
+++ extra_projects/socket_canvas/public/sketch.js
@@ -5,9 +5,20 @@ function setup() {
     background(51)
 
     socket = io.connect('http://localhost:3000')
+
+    socket.on('server',newDrawing)
+    
+}
+
+function newDrawing(data){
+    noStroke()
+    fill(155,0,155)
+    ellipse(data.x,data.y,16,16)
 }
 
 function draw() {
+
+    
     
     //ellipse(mouseX,mouseY,16,16)
 }
@@ -21,9 +32,9 @@ function mouseDragged() {
     }
 
     socket.emit('mouse',data)
-    
+
     noStroke()
     fill (255)
-    ellipse(mouseX, mouseY, 36, 36)
+    ellipse(mouseX, mouseY, 16, 16)
 
 }
--- extra_projects/socket_canvas/server.js
+++ extra_projects/socket_canvas/server.js
@@ -16,4 +16,14 @@ io.sockets.on('connection', newConnection)
 
 function newConnection (socket) {
     console.log("Connection made with " + socket.id)
+
+    socket.on('mouse', mouseMsg)
+
+    function mouseMsg(data) {
+        socket.broadcast.emit('server', data)
+        //io.sockets.emit('server',data) //this will also send data to itself
+        //console.log(data)
+    }
+
+    
 }
