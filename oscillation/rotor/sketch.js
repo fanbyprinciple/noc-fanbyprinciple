2e1aa7cfac88466ec2065792102be8aa72216eb8
fanbyprinciple <fanbyprinciple@gmail.com>
2019-08-16 23:06:47 +0530

    rotor: created

---
+++ oscillation/rotor/sketch.js
@@ -0,0 +1,25 @@
+var angle = 0
+var aVelocity = 0
+var aAcceleration = 0.01
+
+function setup(){
+    createCanvas(400,400)
+
+}
+
+function draw(){
+    background(255)
+
+    fill(175)
+    stroke(0)
+    rectMode(CENTER)
+    translate(width/2,height/2)
+    rotate(angle)
+    line(-50,0,50,0)
+    ellipse(-50,0,8,8)
+    ellipse(50,0,8,8)
+
+    aVelocity += aAcceleration
+    angle += aVelocity
+
+}
