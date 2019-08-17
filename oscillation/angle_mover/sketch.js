312e7df4376e5ba4eb2ce39002261a3e6caad54d
fanbyprinciple <fanbyprinciple@gmail.com>
2019-08-18 00:05:32 +0530

    angle_mover: atan2 not working

--- oscillation/angle_mover/sketch.js
+++ oscillation/angle_mover/sketch.js
@@ -2,7 +2,7 @@
      constructor(mass,x,y){
         this.loc = createVector(width/2,height/2)
         this.vel = createVector(0,0)
-        this.acc = createVector(0.00,0.00)
+        this.acc = createVector(0.01,0.01)
 
         this.mass = mass
         
@@ -21,7 +21,7 @@
         this.aVelocity += this.aAcceleration
         this.angle += this.aVelocity
 
-        this.acc =0
+        //this.acc =0
         this.aAcceleration =0
      }
 
@@ -30,13 +30,14 @@
      }
 
      display() {
+        let i_angle = atan2(this.vel.y,this.vel.x)
         stroke(0)
         fill(175,200)
         rectMode(CENTER)
         push()
         //console.log(this.loc.x,this.loc.y)
         translate(width/2,height/2)
-        rotate(this.angle)
+        rotate(i_angle)
         //console.log(this.loc.x + this.mass*8)
         rect(this.loc.x-width/2,this.loc.y-height/2, this.mass * 16, this.mass * 16)
         pop()
