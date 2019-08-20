ac17445a519c9fda48afb7d79ec6ed0d7772537e
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-08-21 00:04:37 +0530

    cursor_car: added rotation,l eft side trouble

--- oscillation/cursor_car/sketch.js
+++ oscillation/cursor_car/sketch.js
@@ -4,11 +4,17 @@ class Car {
         this.loc = createVector(x,y)
         this.vel = createVector(0,0)
         this.acc = createVector(0,0)
-    }
+    } 
 
     display(){
-       // console.log(this.x,',',this.y,',',this.x+20,',',this.y+20)
-        triangle(this.loc.x, this.loc.y, this.loc.x + 20, this.loc.y +20, this.loc.x-20, this.loc.y+20);    
+        let angle = atan(this.vel.y/this.vel.x) + PI/2
+        push ()
+        translate(this.loc.x,this.loc.y)
+        rotate (angle)
+        // console.log(this.x,',',this.y,',',this.x+20,',',this.y+20)
+        //triangle(this.loc.x, this.loc.y, this.loc.x + 20, this.loc.y +20, this.loc.x-20, this.loc.y+20);    
+        triangle(0,0,20,20,-20,20)
+        pop ()
     }
 
     update() {
@@ -47,11 +53,12 @@ function keyPressed(){
 let car
 function setup(){
     createCanvas(400,400)
-    background(0)
+    
     car=new Car(width/2,height/2)
-    triangle(20,20,21,40,31,53)
+    //triangle(20,20,21,40,31,53)
 }
 function draw() {
+    background(0)
     car.update()
     car.display()
 }
