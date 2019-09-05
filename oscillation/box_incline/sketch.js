48f5621b09e11e1fdbe201af566bb566d23fa673
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-09-05 23:21:55 +0530

    box_incline: better code

--- oscillation/box_incline/sketch.js
+++ oscillation/box_incline/sketch.js
@@ -1,30 +1,42 @@
 class Box {
-    constructor(x,y,size) {
+    constructor(x,y,size,angle) {
         this.loc = createVector(x,y)
         this.size = size
         this.vel =  createVector(0,0)
+        this.angle = angle
 
     }
 
     display() {
-        fill(51)
+        push()
+        rotate (this.angle)
+        fill(255)
         rect(this.loc.x,this.loc.y,this.size,this.size)
+        pop()
     }
 
     move() {
-     //   this.x = 
+        let gravity = createVector(0,-1 * sin (this.angle))
+        this.vel.add(gravity)
+        this.loc.add(this.vel)
+        this.vel.mult(0)
+
+
     }
 }
 
 
 
-var box
+var boxy
 function setup() {
-    createCanvas(100,100)
-    box = new Box(width/2,height/2,20)
+
+    createCanvas(400,400)
+    boxy = new Box(width/2,height/2,40,PI/4)
 }
 
 function draw() {
-    box.display()
-    line (0,height,width,0)
+    background(0)
+    boxy.display()
+    boxy.move()
+    //line (0,height,width,0)
 }
