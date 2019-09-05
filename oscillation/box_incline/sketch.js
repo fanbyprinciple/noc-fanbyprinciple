b63a0710609a444df4c45cd03891d282c1c80e38
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-09-05 23:41:20 +0530

    box_incline: created a scrapy simulation

--- oscillation/box_incline/sketch.js
+++ oscillation/box_incline/sketch.js
@@ -9,14 +9,18 @@ class Box {
 
     display() {
         push()
+        rectMode(CENTER)
+        translate(-94,-94)
         rotate (this.angle)
-        fill(255)
+        fill(51)
+
+    
         rect(this.loc.x,this.loc.y,this.size,this.size)
         pop()
     }
 
     move() {
-        let gravity = createVector(0,-1 * sin (this.angle))
+        let gravity = createVector(0,1 * sin (this.angle))
         this.vel.add(gravity)
         this.loc.add(this.vel)
         this.vel.mult(0)
@@ -25,18 +29,26 @@ class Box {
     }
 }
 
-
+function drawLine(angle) {
+    push()
+    translate(width/2,height/2)
+    rotate(PI/2)
+    line(width,height,0,0)
+    line(-width,-height,width,height)
+    pop()
+}
 
 var boxy
 function setup() {
 
     createCanvas(400,400)
-    boxy = new Box(width/2,height/2,40,PI/4)
+    boxy = new Box(width,0,30,PI/4)
 }
 
 function draw() {
-    background(0)
+    background(255)
     boxy.display()
+    drawLine()
     boxy.move()
     //line (0,height,width,0)
 }
