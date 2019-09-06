b161a5a7c5d404a2c5a6c58a76dcdb802682923d
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-09-06 06:23:33 +0530

    hookes_spring: unfinished business

---
+++ oscillation/hookes_spring/sketch.js
@@ -0,0 +1,19 @@
+class Spring {
+    constructor(length,k){
+        this.anchor = createVector(width/2,0)
+        this.loc = createVector(0,0)
+        this.restLength = length
+        this.k = k
+    }
+
+    calculateForce(){
+    }
+
+    applyForce(){}
+}
+
+
+function draw(){
+    let gravity = createVector(0,1)
+
+}
