9c480c431a237d2fb310846f3f467cd29ee4d8f8
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-09-07 19:34:01 +0530

    hookes_spring: update

--- oscillation/hookes_spring/sketch.js
+++ oscillation/hookes_spring/sketch.js
@@ -1,15 +1,37 @@
 class Spring {
-    constructor(length,k){
+    
+    constructor(length,k,x,y){
         this.anchor = createVector(width/2,0)
-        this.loc = createVector(0,0)
+        this.loc = createVector(x,y)
         this.restLength = length
         this.k = k
     }
 
-    calculateForce(){
+    connectBob(b) {
+        let force = this.loc.sub(this.anchor)
+        let d = force.mag()
+        let stretch = d - this.restLength
+
+
+        force.normalize()
+        force.mult(-1 * this.k * stretch)
+
+        b.applyForce
+        
     }
 
     applyForce(){}
+
+    display() {
+        fill (100)
+        rectMode(CENTER)
+        rect(anchor.x,anchor.y,10,10)
+    }
+
+    displayline(b) {
+        stroke (255)
+        line (b.location.x,b.location.y,anchor.x,anchor.y)
+    }
 }
 
 
