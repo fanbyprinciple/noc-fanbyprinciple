a9b71ae893510be81f6ea0ae5233841b2a2fc34f
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-09-08 16:47:08 +0530

    hookes_spring: spring connected to bob and going down with gravity

--- oscillation/hookes_spring/sketch.js
+++ oscillation/hookes_spring/sketch.js
@@ -1,41 +1,109 @@
+
+
+
+
 class Spring {
-    
-    constructor(length,k,x,y){
+    constructor(l){
         this.anchor = createVector(width/2,0)
-        this.loc = createVector(x,y)
-        this.restLength = length
-        this.k = k
-    }
-
-    connectBob(b) {
-        let force = this.loc.sub(this.anchor)
-        let d = force.mag()
-        let stretch = d - this.restLength
-
-
-        force.normalize()
-        force.mult(-1 * this.k * stretch)
-
-        b.applyForce
+        this.restLength = l
+        this.loc =createVector(this.anchor.x,this.restLength)
+        
+        this.k = 0.1
+         
         
     }
 
-    applyForce(){}
+    connect(bob) {
+            let force = p5.Vector.sub(bob.loc,this.anchor)
+            this.loc = bob.loc
+            //this.currentlength = force.mag()
+            let x = this.restLength - this.currentlength
+            force.normalize()
+            force.mult(-1 * this.k * x)
+    }
+    
+    display() {
+        line (this.anchor.x, this.anchor.y, this.loc.x, this.loc.y)
+    }
+
+
+}
+
+class Bob {
+    constructor(x,y,size) {
+        this.loc = createVector(x,y)
+        this.size = 25
+        this.vel = createVector(0,0)
+        this.acc = createVector(0,0)
+    }
 
     display() {
-        fill (100)
-        rectMode(CENTER)
-        rect(anchor.x,anchor.y,10,10)
+        fill (51)
+        ellipse(this.loc.x,this.loc.y, this.size,this.size)
     }
 
-    displayline(b) {
-        stroke (255)
-        line (b.location.x,b.location.y,anchor.x,anchor.y)
+    applyForce(force) {
+        this.acc.add(force)
+        this.vel.add(this.acc)
+        this.loc.add(this.vel)
+        this.acc.mult(0)
     }
 }
 
+let bob
+let spring
 
-function draw(){
-    let gravity = createVector(0,1)
-
+function setup () {
+    createCanvas(400,400 )
+    bob = new Bob(width/2,height/2,25)
+    spring = new Spring(100)
 }
+
+function draw() {
+    background(120)
+    let gravity = createVector(0,0.1)
+    bob.applyForce(gravity)
+    
+    spring.connect(bob)
+    
+    spring.display()
+    bob.display()
+    
+}
+
+// class Spring {
+    
+//     constructor(length,k,x,y){
+//         this.anchor = createVector(width/2,0)
+//         this.loc = createVector(x,y)
+//         this.restLength = length
+//         this.k = k
+//     }
+
+//     connectBob(b) {
+//         let force = this.loc.sub(this.anchor)
+//         let d = force.mag()
+//         let stretch = d - this.restLength
+
+
+//         force.normalize()
+//         force.mult(-1 * this.k * stretch)
+
+//         b.applyForce
+        
+//     }
+
+//     applyForce(){}
+
+//     display() {
+//         fill (100)
+//         rectMode(CENTER)
+//         rect(anchor.x,anchor.y,10,10)
+//     }
+
+//     displayline(b) {
+//         stroke (255)
+//         line (b.location.x,b.location.y,anchor.x,anchor.y)
+//     }
+// }
+
