d0c0990897da492efe10b94e7a1d51c0fea58528
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-09-11 00:49:48 +0530

    particle: uncontrolled rotation

--- particle_systems/particle/Particle.js
+++ particle_systems/particle/Particle.js
@@ -2,21 +2,38 @@ class Particle {
 
     constructor(l) {
         this.loc = l
-        this.acc = createVector()
-        this.vel = createVector()
+        this.acc = createVector(0,0)
+        this.vel = createVector(0,0)
         this.lifespan = 255
+        this.rotate = true
+        this.angle = 0
+        this.angVel = 0.1
     }
 
     update() {
         this.vel.add(this.acc)
         this.loc.add(this.vel)
         this.lifespan -=2
+        //this.acc =0
     }
 
     display() {
-        stroke (0)
-        fill (175)
-        ellipse (this.loc.x,this.loc.y,25,25)
+        if(this.rotate){
+            push ()
+            translate(this.loc.x,this.loc.y)
+            this.angle += this.angVel
+            rotate (this.angle)
+            stroke(0)
+            fill(this.lifespan)
+            ellipse(this.loc.x, this.loc.y,12,0)
+            ellipse(this.loc.x,this.loc.y,8,8)
+            pop ()
+        } else {
+            stroke (0)
+            fill (175)
+            ellipse (this.loc.x,this.loc.y,8,0)
+            ellipse (this.loc.x+7,this.loc.y,8,8)
+        }
     }
 
     isDead () {
@@ -31,4 +48,10 @@ class Particle {
         this.update()
         this.display()
     }
+
+    applyForce(f){
+        this.acc.add(f)
+    }
+
+    
 }
