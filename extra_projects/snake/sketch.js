c5d5baf4eb0e40bc9d7c5bb25519a4a494d3bde4
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-10-15 00:12:22 +0530

    snake :  basic structure

--- extra_projects/snake/sketch.js
+++ extra_projects/snake/sketch.js
@@ -1,7 +1,70 @@
+
+var foodx
+var foody 
+
+class snake {
+    constructor(){
+        this.pos = createVector(width/2, height/2)
+        this.len = 3
+    }
+
+    display(){
+        fill(255)
+        for( let i =0; i < this.len;++ i){
+            ellipse(this.pos.x- i*11, this.pos.y, 12,12)
+        }
+    }
+
+    update(){
+        this.pos.x += 1
+    }
+
+    checkEdges(){
+        if(this.pos.x == width){
+            this.pos.x = 0
+        } else if(this.pos.x == 0) {
+            this.pos.x = width
+        }
+
+        if(this.pos.y == height) {
+            this.pos.y = 0
+        } else if( this.pos.y == 0) {
+            this.pos.y = height
+        }  
+    }
+
+    run(){
+        this.checkEdges()
+        this.update()
+        this.display()
+    }
+}
+
+class food {
+    constructor(){
+        this.pos = createVector(foodx, foody)
+        this.eaten = false
+    }
+
+    display(){
+        fill (0,120,255)
+        ellipse(this.pos.x, this.pos.y, 18,18)
+    }
+}
+
+
 function setup(){
-    createcanvas(400,400)
+    createCanvas(400,400)
+
+    foodx = random(width)
+    foody = random(height)
+    s = new snake()
+    f = new food()
 }
 
 function draw(){
     background(0)
+    s.run()
+    
+    f.display()
 }
