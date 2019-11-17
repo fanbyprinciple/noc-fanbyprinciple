ed85280df90c426ad7b1e20ad9fdb8ef5daeb68d
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-11-18 00:12:25 +0530

    markov_chains : doneto generate small text

--- extra_projects/markov_chains/sketch.js
+++ extra_projects/markov_chains/sketch.js
@@ -1,17 +1,54 @@
-var txt = "Ashish is a funny boy"
-var order = 3
-var ngram = []
+var txt = "Ashish may take a hashish but he ramains Ashish and does not become a hashashin."
+var order = 4
+var ngram = {}
+var button
 
 function setup(){
     noCanvas()
 
     for(var i=0 ; i < txt.length - order+1; ++i){
-        var gram = txt.substring(i,i+3)
-        ngram.push(gram)
+        var gram = txt.substring(i,i+order)
+
+        if(!ngram[gram]){
+            ngram[gram] = []
+        }
+        
+        ngram[gram].push(txt.charAt(i+order))
+        
+        
 
 
     }
 
-    console.log(ngram)
+    button = createButton("generate")
+    button.mousePressed(markovIt)
+
+    
+}
+
+function markovIt(){
+
+    var currentGram = txt.substring(0,order)
+    let next
+    let possibilities
+    let result = currentGram
+
+    for (var i = 0; i <10 ; i ++){
+        
+        possibilities = ngram[currentGram]
+
+        if(!possibilities){
+            break
+        }
+
+        next = random(possibilities)
+        
+        result += next
+        currentGram = result.substring(result.length-order, result.length)
+
+    }
+
+    createP (result)
+
 }
 
