5f7d6179dde88a3b804f62c5f63d70371925e196
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-11-29 07:38:45 +0530

    bubble_sort :created

--- extra_projects/bubble_sort/sketch.js
+++ extra_projects/bubble_sort/sketch.js
@@ -1,34 +1,59 @@
 var values
 
+var i =0
+var j =0
+
 function setup(){
-    createCanvas(800,500)
+    createCanvas(574,526)
 
     values = new Array(width)
 
-    for(  i = 0; i < values.length; i++){
-        values[i] = random(height)
+    for(  k = 0; k < values.length; k++){
+        values[k] = random(height)
     }
 
     //values = sort(values)
 
-    for (i =0; i< values.length ; ++i){
-        for (j =0; j < values.length -i; ++j){
-            if(values[j+ 1] < values[j]){
-                temp = values[j+1]
-                values[j+1] = values[j]
-                values[j] = temp
-            }
-        }
-    }
+    // for (i =0; i< values.length ; ++i){
+    //     for (j =0; j < values.length -i; ++j){
+    //         if(values[j+ 1] < values[j]){
+    //             temp = values[j+1]
+    //             values[j+1] = values[j]
+    //             values[j] = temp
+    //         }
+    //     }
+    // }
 
 }
 
 function draw(){
     background(0)
 
-    for (i=0; i < values.length; i++){
+    
+    print(i, values.length)
+    if (i < values.length ) {
+        for(j =0;j< values.length -i-1; ++j){
+            if(values[j+1] < values[j]){
+                temp =values[j+1]
+                values[j+1] = values[j]
+                values[j] = temp
+            }
+        }
+        
+  
+    } else {
+        console.log("Finished")
+        noLoop()
+
+    }
+
+    i++
+
+    
+    
+    for (z=0; z < values.length; z++){
         stroke(255)
-        line (i, height, i , height - values[i])
+        line (z, height, z , height - values[z])
     }
 
 }
