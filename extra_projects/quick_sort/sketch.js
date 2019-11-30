998454b718d6502aff0187beb2f5a91ddcbffeae
Ashwin Jayaprakash <fanbyprinciple@gmail.com>
2019-11-30 21:04:05 +0530

    quick_sort: created array

---
+++ extra_projects/quick_sort/sketch.js
@@ -0,0 +1,32 @@
+w =20
+
+function setup(){
+    createCanvas(800,200)
+    values = new Array(floor(width/w))
+    for (let i = 0; i < values.length; i++){
+        values[i] = random(height)
+    }
+
+    console.log(values)
+
+    //frameRate(5)
+
+}
+
+function quickSort(arr, start, end) {
+    if(start >= end){
+        return
+    }
+
+    let index = partition(arr, start, end)
+}
+
+function draw() {
+    background(0)
+
+    for (i=0; i <values.length; ++i){
+        stroke (255)
+        fill (55)
+        rect ((i * w), height-values[i], w ,values[i])
+    }
+}
