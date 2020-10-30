aafeffd65f293168b6df8929735c4cf9107ed79a
fanbyprinciple <fanbyprinciple@gmail.com>
2020-10-31 00:57:14 +0530

    zoom call overlay added

---
+++ extra_projects/video_text_overlay/readme.md
@@ -0,0 +1,14 @@
+# Creating a text overlay for zoom
+
+from coding train ! 
+<https://www.youtube.com/watch?v=9z9mbiOZqSs&t=28s>
+
+Steps :
+
+1. Train a teachable machine model
+2. Upload the model and use it on a p5 sketch
+3. Start up obs studio and capture browser 
+4. start virtual camera on OBS
+5. use it on a zoom meeting
+
+![zoom_gif](zoom_hello.gif)
---
+++ extra_projects/video_text_overlay/sketch.js
@@ -0,0 +1,85 @@
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Webcam Image Classification using a pre-trained customized model and p5.js
+This example uses p5 preload function to create the classifier
+=== */
+
+// Classifier Variable
+let classifier;
+// Model URL
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/wnHexVJcb/';
+
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+
+let question
+
+let fadeout = 0
+// Load the model first
+function preload() {
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  question = loadImage('hello.png')
+}
+
+function setup() {
+  createCanvas(320, 260);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  video.hide();
+
+  flippedVideo = ml5.flipImage(video)
+  // Start classifying
+  classifyVideo();
+}
+
+function draw() {
+  background(0);
+  // Draw the video
+  tint(255)
+  image(flippedVideo, 0, 0);
+  
+  if(label == 'Hello'){
+    fadeout = 255
+    
+  }
+  
+  fadeout -= 10
+  if(fadeout>10){
+    tint(255,fadeout)
+    image(question,100,0)
+  }
+  // Draw the label
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text(label, width / 2, height - 4);
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video)
+  classifier.classify(flippedVideo, gotResult);
+}
+
+// When we get a result
+function gotResult(error, results) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
+  // Classifiy again!
+  classifyVideo();
+}
