850aa5c4cb1fc958e7fe35124cc6b9041aecfd46
fanbyprinciple <fanbyprinciple@gmail.com>
2020-12-13 13:56:49 +0530

    fireworks added

---
+++ extra_projects/firework_wish/sketch.js
@@ -0,0 +1,30 @@
+let fireworks = []
+let gravity
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB)
+  strokeWeight(4)
+  gravity =  createVector(0,0.2)
+  stroke(255)
+  background(0)
+}
+
+function draw() {
+  colorMode(RGB)
+  background(0,0,0,25);
+  
+  if(random(1) < 0.03){
+    fireworks.push(new Firework())  
+  }
+  
+  for ( let i = fireworks.length -1; i>=0 ; i--){
+    fireworks[i].update()
+    fireworks[i].show() 
+    
+  if(fireworks[i].done()){
+    fireworks.splice(i,1)
+  }    
+  }
+  
+}
